1950163e0620c75b9f7a675fd769c853b3d696b0
thaithestar <thaithestar@gmail.com>
2016-02-19 01:47:03 -0600

    Need to implement the fragments

--- app/src/main/java/com/example/thait/quiz/MainActivity.java
+++ app/src/main/java/com/example/thait/quiz/MainActivity.java
@@ -1,19 +1,20 @@
 package com.example.thait.quiz;
 
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static int points = 0;
-
+    Button bn;
     public static void success(double point){
         points += point;
     }
@@ -22,45 +23,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void next(View view) {
-        Intent intent = new Intent(this, Frag1.class);
+        Intent intent = new Intent(this, Q1.class);
         startActivity(intent);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        bn = (Button) findViewById(R.id.button);
+//        bn.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                FragmentManager fragmentManager = getFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//
+//                FragQ1 frag1 = new FragQ1();
+//
+////                fragmentManager.add(R.id.fragment_container,frag1);
+////                fragmentTransaction.commit();
+////                FragQ2 frag2 = new FragQ2();
+////                fragmentManager.add(R.id.fragment_container,frag2);
+//
+//
+//            }
+//
+//        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
